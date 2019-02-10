b59484fac40de6132ea1a802a57263652899a1f7
Tomas Jasek <tomsik68@gmail.com>
2019-02-09 21:02:30 -0500

    Make number of VMs per file configurable
    via CLI flag

--- service/service.go
+++ service/service.go
@@ -10,12 +10,8 @@ import (
 	"net"
 )
 
-const (
-	vmPerFile = 500
-)
-
 // Serve starts grpc server on ip:port, optionally using tls. If *tls == true, then *certFile and *keyFile must be != null
-func Serve(ip *string, port *uint, tls *bool, certFile *string, keyFile *string, outDir *string, templatesDir *string) error {
+func Serve(ip *string, port *uint, tls *bool, certFile *string, keyFile *string, outDir *string, templatesDir *string, vmPerFile *uint64) error {
 	server, err := net.Listen("tcp", fmt.Sprintf("%s:%d", *ip, *port))
 	if err != nil {
 		return err
@@ -32,7 +28,7 @@ func Serve(ip *string, port *uint, tls *bool, certFile *string, keyFile *string,
 
 	grpcServer := grpc.NewServer(opts...)
 
-	wr := consumer.NewTemplateGroupWriter(*outDir, *templatesDir, vmPerFile)
+	wr := consumer.NewTemplateGroupWriter(*outDir, *templatesDir, *vmPerFile)
 	goat_grpc.RegisterAccountingServiceServer(grpcServer, importer.NewAccountingServiceImpl(wr, wr, wr))
 
 	return grpcServer.Serve(server)
